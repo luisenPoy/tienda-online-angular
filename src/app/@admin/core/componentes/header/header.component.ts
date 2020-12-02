ae3235869f780e9bb11441d1fcb0db696b242c53
Luisen Poy <kttweb@gmail.com>
2020-12-02 18:59:10 +0100

    conversion plantilla Simple Sidebar de startbootstrap

---
+++ src/app/@admin/core/componentes/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  toggledValue = true;
+  @Output() toggleChange = new EventEmitter<boolean>();
+
+  toggle() {
+    if (this.toggledValue === undefined) {
+      this.toggledValue = true;
+    }
+
+    this.toggledValue = !this.toggledValue;
+
+    console.log(this.toggledValue);
+
+    this.toggleChange.emit(this.toggledValue);
+  }
+}
